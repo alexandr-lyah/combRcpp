f77293cb2bfc409be21ac5c5977c5667fb45c1bf
jmcimula <jmkacim@gmail.com>
2016-06-03 11:52:44 +0200

    Function Cpp in R

---
+++ CppR.cpp
@@ -0,0 +1,23 @@
+library(Rcpp)
+cppFunction('NumericVector rowSumsC(NumericMatrix x) {
+  int nrow = x.nrow(), ncol = x.ncol();
+  NumericVector out(nrow);
+
+  for (int i = 0; i < nrow; i++) {
+    double total = 0;
+    for (int j = 0; j < ncol; j++) {
+      total += x(i, j);
+    }
+    out[i] = total;
+  }
+  return out;
+}
+double sumC(NumericVector x) {
+  int n = x.size();
+  double total = 0;
+  for(int i = 0; i < n; ++i) {
+    total += x[i];
+  }
+  return total;
+}
+')
