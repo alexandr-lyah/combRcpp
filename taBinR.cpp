2824fd45092fb0d2568112ff884aa4ea819fed0c
jmcimula <jmkacim@gmail.com>
2016-06-04 12:42:15 +0200

    largest and smallest element in the array

---
+++ taBinR.cpp
@@ -0,0 +1,23 @@
+library(Rcpp)
+#Biggest value in the column
+cppFunction('
+double bigVal(NumericVector x) {
+  int n = x.size(), index = 0;
+  
+  for(int i = 0; i < n; ++i) {
+				if(x[index] < x[i]){ index=i;}
+  }
+  return x[index];
+}
+')
+#Smallest value in the column
+cppFunction('
+double smallVal(NumericVector x) {
+  int n = x.size(), index = 0;
+  
+  for(int i = 0; i < n; ++i) { 
+				if(x[index] > x[i]){ index=i;}
+  }
+  return x[index];
+}
+')
